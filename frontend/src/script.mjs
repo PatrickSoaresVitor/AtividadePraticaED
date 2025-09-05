57f39bda2b91fc54477009c26c8fdb55628b6822
PATRICK SOARES VITOR <161238904+PatrickSoaresVitor@users.noreply.github.com>
2025-09-04 22:13:59 -0300

    s

--- frontend/src/script.mjs
+++ frontend/src/script.mjs
@@ -1,3 +1,11 @@
 function funcao(){
-    return "cliquei"
+    let numero1 = Number(document.getElementById("numero1").value)
+    let numero2 = (document.getElementById("numero2").value)
+    let sub = numero1
+    let sub2 = numero2
+    //alert("a subtração é " + sub)  antigo//
+    // let é local e var é global//
+    alert(`A subtração é ${sub}`)
+    alert(`A subtração é ${sub2}`)
+    
 }
